f16db1f012c838cfa257ff688e4988a15d2de2e2
Jeferson Rios <jefry1795@hotmail.com>
2026-03-24 11:58:45 -0500

    Update main.py

--- control_ingreso/main.py
+++ control_ingreso/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.router import users, auth, person, sede, center, equipments, equipments_sede, access, roles, autorizacion_salida, movements, permisos, department, type, modulo, categorie, area, cities
+from app.router import users, auth, person, sede, center, equipments, equipments_sede, access, roles, autorizacion_salida, movements, permisos, department, type, modulo, categorie, area, cities, encuestas
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
@@ -31,7 +31,7 @@ app.include_router(modulo.router, prefix="/modulo", tags=["modulos"])
 app.include_router(categorie.router, prefix="/categoria", tags=["categorias"])
 app.include_router(area.router, prefix="/area", tags=["areas"])
 app.include_router(cities.router, prefix="/cities", tags=["cities"])
-
+app.include_router(encuestas.router, prefix="/encuesta", tags=["encuesta"])
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 @app.get("/")
